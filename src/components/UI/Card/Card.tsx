7e0996271b309560e55811277a26ab1fb0d64da3
marco94gug <marco.guglielmino94@gmail.com>
2025-03-08 14:45:13 +0100

    feat: improve card style

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,20 +1,53 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Post } from "../../../types";
 import styles from "./card.module.scss";
 
 export default function Card({ post }: { post: Post }) {
-    const body = useMemo(() => {
-        const regex = /^[\s\S]{0,64}/;
+  const [isShown, setIsShown] = useState(false);
+  const [isTextLonger, setTextLonger] = useState(false);
 
-        return post.body.match(regex)?.[0];
-      }, [post.body]);
+  const handleToggleBody = () => {
+    setIsShown(!isShown);
+  };
 
+  const body = useMemo(() => {
+    const regex = /^[\s\S]{0,64}/;
+    const firstString = post.body.match(regex)?.[0];
+
+    if (firstString!.length < post.body.length) {
+      setTextLonger(true);
+    }
+
+    if (firstString!.length < post.body.length && !isShown) {
+      return `${firstString}...`;
+    } else if (firstString!.length < post.body.length && isShown) {
+      return post.body;
+    }
+
+    return `${firstString}`;
+  }, [post.body, isShown]);
+
+  const showButtonText = useMemo(() => {
+    return isShown ? "show less" : "show more";
+  }, [isShown]);
+
+  const printShowBtn = () => {
+    if (isTextLonger) {
+      return (
+        <span onClick={handleToggleBody} className={styles["card__body--show"]}>
+          {showButtonText}
+        </span>
+      );
+    }
+  };
 
   return (
     <div className={styles.card}>
       <h2>{post.name}</h2>
       <p className={styles.card__email}>{post.email}</p>
-      <p>{body}</p>
+      <p>
+        {body} {printShowBtn()}
+      </p>
     </div>
   );
 }
